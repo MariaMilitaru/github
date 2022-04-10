84fc6c46cc363d21f66c4f39eea1fc4abad606d5
SebastianMarius <sebastianmarius2100@gmail.com>
2022-04-10 16:51:57 +0300

    i did a good job  :+1:

--- github/src/App.js
+++ github/src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import UserPage from './container/UserPage';
+import {
+    BrowserRouter as Router,
+    Route,
+    Redirect,
+    BrowserRouter,
+    Routes,
+} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <UserPage></UserPage>
-    </div>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<UserPage />} />
+                    {/* <Route path='/home' element{<HomePage/>}/> */}
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
