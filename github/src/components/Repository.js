4f529038e3801827f9942017bb8af6472fc5ca70
Maria Militaru <mariaadmili@gmail.com>
2022-04-11 14:56:10 +0300

    add css to repo

--- github/src/components/Repository.js
+++ github/src/components/Repository.js
@@ -1,11 +1,21 @@
 import React from "react";
+import '../styles/Repository.css';
+import { Card, Grid } from '@mui/material';
+import CollectionsBookmarkIcon from '@mui/icons-material/CollectionsBookmark';
+import CodeOutlinedIcon from '@mui/icons-material/CodeOutlined';
+import ForkRightOutlinedIcon from '@mui/icons-material/ForkRightOutlined';
 
 export default function Repository({ repo }) {
   return (
-    <>
-      <div>{repo.full_name}</div>
-      <div>Created at: {repo.created_at}</div>
-      <div>Lang: {repo.language}</div>
-    </>
+    <Card  sx={{ minWidth: 330 }} className="repo_card">
+      <Grid container direction="row" alignItems="center">
+        <CollectionsBookmarkIcon fontSize="small"/> <p className="repo_info"> {repo.name}  </p>
+      </Grid>
+      <Grid container direction="row" alignItems="center">
+        {repo.language && <CodeOutlinedIcon  fontSize="small"/> }
+        {repo.language && <p className="repo_info" > {repo.language}  </p>}
+        <ForkRightOutlinedIcon  fontSize="small"/> <p className="repo_info"> {repo.forks_count}  </p>
+      </Grid>
+    </Card>
   );
 }
